1394db60aefccc6062e1c31b55d56a7b879c56ed
webtech <websportslogic@gmail.com>
2026-01-11 22:37:46 +0530

    Step 1 Test Pass: empty string returns 0

---
+++ lib/calculator.dart
@@ -0,0 +1,9 @@
+class StringCalculator
+{
+  int add(String numbers){
+
+    if(numbers.isEmpty) return 0; // KATA  Step 1.1
+
+    return 0;
+  }
+}
---
+++ test/calculator_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:string_calculator_tdd/calculator.dart';
+
+void main() {
+  late StringCalculator calculator;
+  // Initialize calculator before each test
+  setUp(() {
+    calculator = StringCalculator();
+  });
+  // STEP 1
+  test('empty string returns 0', () {
+    expect(calculator.add(''), 0);
+  });
+
+
+}
